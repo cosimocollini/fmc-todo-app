a9b5a6d0b2c49d6de2d0b7323a2c51f90823da1e
Cosimo Collini <cosimocollini@Cosimos-MBP.lan>
2021-05-19 23:14:09 +0200

    added deploy config

--- webpack.common.js
+++ webpack.common.js
@@ -3,20 +3,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, './index.js'),
+  entry: path.resolve(__dirname, "./index.js"),
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js',
-    publicPath: '/',
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+    publicPath: "/fmc-todo-app/",
   },
   plugins: [
     new CleanWebpackPlugin(),
 
     new HtmlWebpackPlugin({
-      title: 'Sdg',
-      favicon: path.resolve(__dirname, './images/favicon-32x32.png'),
-      template: path.resolve(__dirname, './index.html'),
-      filename: 'index.html',
+      title: "Sdg",
+      favicon: path.resolve(__dirname, "./images/favicon-32x32.png"),
+      template: path.resolve(__dirname, "./index.html"),
+      filename: "index.html",
     }),
   ],
   module: {
@@ -24,10 +24,13 @@ module.exports = {
       {
         test: /\.(scss|css)$/,
         use: [
-          'style-loader',
-          {loader: 'css-loader', options: {sourceMap: true, importLoaders: 1}},
-          {loader: 'postcss-loader', options: {sourceMap: true}},
-          {loader: 'sass-loader', options: {sourceMap: true}},
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { sourceMap: true, importLoaders: 1 },
+          },
+          { loader: "postcss-loader", options: { sourceMap: true } },
+          { loader: "sass-loader", options: { sourceMap: true } },
         ],
       },
       {
@@ -36,16 +39,17 @@ module.exports = {
           loader: "file-loader",
           options: {
             name: "/images/[name].[ext]",
-          }
-        }
+          },
+        },
       },
       {
         test: /\.woff(2)?(\?[a-z0-9]+)?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff"
-      }, {
+        loader: "url-loader?limit=10000&mimetype=application/font-woff",
+      },
+      {
         test: /\.(ttf|eot)(\?[a-z0-9]+)?$/,
-        loader: "file-loader"
-      }
-    ]
-  }
+        loader: "file-loader",
+      },
+    ],
+  },
 };
